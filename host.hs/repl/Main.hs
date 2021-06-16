f97fdf49b4dc52cb0860dcdb5bd6a1839951bd60
ComplYue <compl.yue@icloud.com>
2021-06-16 22:03:23 +0800

    repl upd

---
+++ host.hs/repl/Main.hs
@@ -0,0 +1,46 @@
+module Main where
+
+-- import           Debug.Trace
+
+import Control.Concurrent
+import Control.Exception
+import Control.Monad (void)
+import qualified Data.Text as T
+import GHCi.Signals
+import Language.Edh.EHI
+import Repl
+import System.Environment (getArgs)
+import System.Exit (exitFailure)
+import System.IO (hPutStrLn, stderr)
+import Prelude
+
+main :: IO ()
+main = do
+  -- don't crash on double Ctrl^C or Ctrl^\, mimic what GHCi is doing
+  installSignalHandlers
+  -- run the specified module, assuming `repl` semantics
+  getArgs >>= \case
+    [] -> runModu "curry"
+    [edhModu] -> runModu edhModu
+    _ -> hPutStrLn stderr "Usage: cedh [ <edh-module> ]" >> exitFailure
+  where
+    runModu :: FilePath -> IO ()
+    runModu !moduSpec = do
+      !console <- defaultEdhConsole defaultEdhConsoleSettings
+      let !consoleOut = consoleIO console . ConsoleOut
+
+      void $
+        forkFinally (edhProgLoop moduSpec console) $ \ !result -> do
+          case result of
+            Left (e :: SomeException) ->
+              consoleOut $ "💥 " <> T.pack (show e)
+            Right _ -> pure ()
+          -- shutdown console IO anyway
+          consoleIO console ConsoleShutdown
+
+      consoleOut ">> Currying Đ (Edh) <<\n"
+      consoleOut
+        "* Blank Screen Syndrome ? Take the Tour as your companion, checkout:\n"
+      consoleOut "  https://github.com/e-wrks/cedh/tree/master/Tour\n"
+
+      consoleIOLoop console
